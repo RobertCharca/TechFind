9634167e52f8ee45ab2eae91c21f860f22acdfeb
Robert Charca <trevoraobac@gmail.com>
2022-06-03 20:30:17 -0500

    Se agregaron nuevos modelos.

---
+++ app/Models/comentarioProducto.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class comentarioProducto extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/detalleProducto.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class detalleProducto extends Model
+{
+    use HasFactory;
+}
