72ba5d8f9de314f809e4d3658d490dfc27506339
Robert Charca <trevoraobac@gmail.com>
2022-06-10 00:14:39 -0500

    Controllers: Nuevo controlador de usuarios agregado.

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+
+class UsuarioController extends Controller
+{
+    //
+    public function index(){
+        $usuarios = Usuario::All();
+        return view('usuarios.usuarioTable')->with('usuarios', $usuarios);
+    }
+
+    public function agregarUsuario(){
+        return view('usuarios.agregarUsuario');
+    }
+
+    public function insertarUsuario(Request $request){
+        //dd($request);
+        $usuario = new Usuario();
+        $usuario->nombre = $request->get('nombre');
+        $usuario->apellidos = $request->get('apellidos');
+        $usuario->username = $request->get('username');
+        $usuario->email = $request->get('email');
+        $usuario->password = $request->get('password');
+        $usuario->pais = $request->get('pais');
+        $usuario->ciudad = $request->get('ciudad');
+
+        $usuario->save();
+        return redirect('usuario');
+    }
+
+    public function actualizarUsuario($id){
+        $usuario = Usuario::find($id);
+        //dd($usuarioedit);
+        return view('usuarios.actualizarUsuario')->with('usuarios', $usuario);
+    }
+
+    public function updateUsuario(Request $request, $id){
+        $usuario = Usuario::find($id);
+        $usuario->nombre = $request->nombre;
+        $usuario->apellidos = $request->apellidos;
+        $usuario->username = $request->username;
+        $usuario->email = $request->email;
+        $usuario->password = $request->password;
+        $usuario->pais = $request->pais;
+        $usuario->ciudad = $request->ciudad;
+
+        $usuario->save();
+        return redirect('/usuario');
+    }
+
+    public function eliminarUsuario(Request $request ,$id){
+        //echo $id;
+        $usuario = Usuario::find($id);
+        $usuario->delete();
+        return redirect('/usuario');
+    }
+}
