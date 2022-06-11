4311bb98cd85a5048b820e54e9f8960d2e9118be
Robert Charca <trevoraobac@gmail.com>
2022-06-10 21:22:52 -0500

    Views: Rutas del lab 12 eliminados.

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/productos', [App\Http\Controllers\ProductoController::class, 'index']);
-Route::get('/productos/create', [App\Http\Controllers\ProductoController::class, 'create']);
-Route::post('/productos/insert', [App\Http\Controllers\ProductoController::class, 'insert']);
-Route::get('/productos/{id}/edit', [App\Http\Controllers\ProductoController::class, 'edit']);
-Route::post('/productos/update/{id}', [App\Http\Controllers\ProductoController::class, 'update']);
-Route::get('/productos/{id}/delete', [App\Http\Controllers\ProductoController::class, 'delete']);
-
-Route::get('/categorias', [App\Http\Controllers\CategoriaController::class, 'index']);
-Route::get('categorias/create', [App\Http\Controllers\CategoriaController::class, 'create']);
-Route::post('categorias/insert', [App\Http\Controllers\CategoriaController::class, 'insert']);
-Route::get('/categorias/{id}/edit', [App\Http\Controllers\CategoriaController::class, 'edit']);
-Route::post('/categorias/update/{id}', [App\Http\Controllers\CategoriaController::class, 'update']);
-Route::get('/categorias/{id}/delete', [App\Http\Controllers\CategoriaController::class, 'delete']);
