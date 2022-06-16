f26460e5c8bfb4e8c7c02b32caab78de43d04407
Robert Charca <trevoraobac@gmail.com>
2022-06-16 01:12:55 -0500

    Views: Nuevos archivos relacionados a Excel y Pdf para usuarios.

---
+++ resources/views/excel/usuarioExcel.blade.php
@@ -0,0 +1,24 @@
+<table>
+    <thead>
+    <tr>
+        <th>Nombres</th>
+        <th>Apellidos</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Pais</th>
+        <th>Ciudad</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($usuarios as $usuario)
+        <tr>
+            <td>{{ $usuario->nombre }}</td>
+            <td>{{ $usuario->apellidos }}</td>
+            <td>{{ $usuario->nombre_usuarios }}</td>
+            <td>{{ $usuario->email }}</td>
+            <td>{{ $usuario->pais }}</td>
+            <td>{{ $usuario->ciudad }}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
