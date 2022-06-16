d076792f0dc7d2c7f4affb14cf2d04813463c2b5
Robert Charca <trevoraobac@gmail.com>
2022-06-16 01:12:06 -0500

    Controllers: Metodo export y createPDF agregados.

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Usuario;
+use App\Exports\UsuarioExport;
+use Maatwebsite\Excel\Facades\Excel;
+use PDF;
 
 class UsuarioController extends Controller
 {
@@ -58,4 +61,19 @@ class UsuarioController extends Controller
         $usuario->delete();
         return redirect('/usuario');
     }
+
+    public function export()
+    {
+        return Excel::download(new UsuarioExport, 'usuarios.xlsx');
+    }
+
+    public function createPDF(){
+        $usuarios = Usuario::all();
+        $pdf = PDF::loadView('pdf.usuarioPdf', [
+            'usuarios' => $usuarios
+        ]);
+        // download PDF file with download method
+        return $pdf->download('usuarios.pdf');
+    }
+
 }
