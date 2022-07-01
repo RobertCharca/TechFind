18da7c4a9c583eb76ae478dc9411f164e7675c31
Robert Charca <trevoraobac@gmail.com>
2022-07-01 04:14:36 -0500

    Cambios desconocidos.

--- techfind-interface/src/router/index.js
+++ techfind-interface/src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+//Pagina principal
 import main from '../views/MainView.vue'
-import Login from '../views/auth/LoginView.vue'
+//Login y registro
+import Login from '../components/user/LoginComponent.vue'
+import Registro from '../components/user/RegisterComponent.vue'
 
 Vue.use(VueRouter)
 
@@ -14,55 +17,73 @@ const routes = [{
 
     {
         path: '/user',
-        name: 'user',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
+        name: 'user-component',
         component: () =>
-            import ( /* webpackChunkName: "user" */ '../views/user/UserView.vue')
+            import ( /* webpackChunkName: "user" */ '../components/user/UserComponent'),
+        //Rutas anidadas
+        children:[
+            {
+                path: 'general',
+                name: 'general-component',
+                component: () =>
+                    import('../components/user/profile/GeneralComponent.vue')
+            },
+            {
+                path: 'posts',
+                name: 'publicacion-component',
+                component: () =>
+                    import('../components/user/profile/PublicacionesComponent.vue')
+            },
+            {
+                path: 'photos',
+                name: 'fotos-component',
+                component: () =>
+                    import('../components/user/profile/FotosComponent.vue')
+            },
+        ]
+    },
+    {
+        path: '/user/edit',
+        name: 'useredit-component',
+        component: () =>
+            import ( /* webpackChunkName: "user" */ '../components/user/profile/EditarPerfilComponent.vue'),
     },
     {
         path: '/vendedor',
         name: 'vendedor',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "user" */ '../views/business/VendedorView.vue')
     },
     {
         path: '/colaboradores',
         name: 'colaboradores',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "user" */ '../views/user/ColaboradoresView.vue')
     },
     {
         path: '/catalogo',
         name: 'catalogo',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "user" */ '../views/business/CatalogoView.vue')
     },
     {
         path: '/producto',
         name: 'producto',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "user" */ '../views/business/ProductoView.vue')
     },
+    //Login y registro de usuario
     {
-        path: '/Login',
+        path: '/login',
         name: 'login',
         component: Login
 
     },
+    {
+        path: '/register',
+        name: 'register',
+        component: Registro
+    }
 
 ]
 
