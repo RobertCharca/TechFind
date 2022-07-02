d08c0cc314bc95358cfee928dafcc7f44966d8d9
Robert Charca <trevoraobac@gmail.com>
2022-07-01 20:51:31 -0500

    Components: Componentes de Google Maps agregado.

--- techfind-interface/src/components/business/location/GoogleMapsComponent.vue
+++ techfind-interface/src/components/business/location/GoogleMapsComponent.vue
@@ -1,13 +1,34 @@
 <template>
-
+    <gmap-map
+        :center="center"
+        :zoom="16"
+    >
+        <gmap-marker
+            :key="index"
+            v-for="(m, index) in markers"
+            :position="m.position"
+            :title="m.title"
+            :clickable="true"
+            :draggable="true"
+            @click="center=m.position"
+        ></gmap-marker>
+    </gmap-map>
 </template>
-
 <script>
 export default {
-    name: "GoogleMapsComponent.vue"
+    data () {
+        return {
+            center: {lat: this.latitude, lng: this.longitude},
+            markers: [{
+                position: {lat: this.latitude, lng: this.longitude},
+                title: this.title
+            }]
+        }
+    },
+    props: {
+        latitude: Number,
+        longitude: Number,
+        title: String
+    }
 }
 </script>
-
-<style scoped>
-
-</style>
